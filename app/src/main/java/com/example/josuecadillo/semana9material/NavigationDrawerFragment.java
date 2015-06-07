18e3ab4f3b1fa70f9d6ee562ff27e70bfcc3f8f3
josuecadillo <jfcadillo951@gmail.com>
2015-06-07 17:44:30 -0500

    Touch Events on Navigationdrawer

--- app/src/main/java/com/example/josuecadillo/semana9material/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/josuecadillo/semana9material/NavigationDrawerFragment.java
@@ -11,9 +11,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.josuecadillo.semana9material.adapter.CustomItemAdapter;
 
@@ -54,7 +58,18 @@ public class NavigationDrawerFragment extends Fragment {
         adapter = new CustomItemAdapter(getActivity(),getData());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addOnItemTouchListener(new RecyclerViewTouchListener(getActivity(),recyclerView,new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(),"onClick"+position,Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getActivity(),"onLongClick"+position,Toast.LENGTH_SHORT).show();
+
+            }
+        }));
         return layout;
     }
 
@@ -102,8 +117,10 @@ public class NavigationDrawerFragment extends Fragment {
 
     public static List<CustomItem> getData(){
         List<CustomItem> data = new ArrayList<>();
-        int[] iconList = {R.drawable.ic_number1,R.drawable.ic_number2,R.drawable.ic_number3,R.drawable.ic_number4};
-        String[] stringList = {"Opcion 1","Opcion 2","Opcion 3","Opcion 4"};
+        int[] iconList = {R.drawable.ic_number1,R.drawable.ic_number2,R.drawable.ic_number3,R.drawable.ic_number4,
+                R.drawable.ic_number1,R.drawable.ic_number2,R.drawable.ic_number3,R.drawable.ic_number4,
+                R.drawable.ic_number1,R.drawable.ic_number2,R.drawable.ic_number3,R.drawable.ic_number4};
+        String[] stringList = {"Opcion 1","Opcion 2","Opcion 3","Opcion 4","Opcion 5","Opcion 6","Opcion 7","Opcion 8","Opcion 9","Opcion 10","Opcion 11","Opcion 12"};
         for (int i = 0; i < iconList.length && i< stringList.length; i++) {
             CustomItem current = new CustomItem();
             current.setTitle(stringList[i]);
@@ -123,5 +140,60 @@ public class NavigationDrawerFragment extends Fragment {
     public static String readFromPreferences(Context context,String preferenceName,String defaultValue){
         SharedPreferences sharedPrefences = context.getSharedPreferences(PREF_FILE_NAME,context.MODE_PRIVATE);
         return sharedPrefences.getString(preferenceName,defaultValue);
+    }
+
+
+
+    class RecyclerViewTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecyclerViewTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener) {
+            Log.d("Out", "constructor de RecyclerViewTouchListener");
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    Log.d("Franco","onSingleTapUp"+e);
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    Log.d("Franco","onLongPress"+e);
+                    View child = recyclerView.findChildViewUnder(e.getX(),e.getY());
+                    if (child != null && clickListener!=null){
+                        clickListener.onLongClick(child,recyclerView.getChildLayoutPosition(child));//recyclerView.getChildPosition(child));
+                    }
+                    //super.onLongPress(e);
+                }
+            });
+
+
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(),e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e))
+            {
+                clickListener.onClick(child,rv.getChildLayoutPosition(child)); //rv.getChildPosition(child));
+            }
+            Log.d("Out", "onInterceptTouchEvent " + gestureDetector.onTouchEvent(e));
+
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+        }
+    }
+    public static interface ClickListener{
+        public void onClick(View view,int position);
+        public void onLongClick(View view,int position);
     }
 }
